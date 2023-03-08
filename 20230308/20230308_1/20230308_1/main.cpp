c026aa417ecf1798105f34e9d37f410d9cb780c2
seongho2 <125865161+seongho2@users.noreply.github.com>
2023-03-08 09:49:29 +0900

    Add files via upload

---
+++ 20230308/20230308_1/20230308_1/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>  //  header 파일 추가 standard IO
+
+
+int main (void) 
+{
+
+    printf("teat  \n");  //\n line feed
+    printf("202315012  \n");  //\n line feed
+    printf("Seong Ho AN  \n");  //\n line feed
+
+    return 0;
+}
+
+//  ctrl+F5 는 실행
