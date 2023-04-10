5435f2484d7d4cec9798c12617a5427028ca0a47
seongho2 <125865161+seongho2@users.noreply.github.com>
2023-04-10 22:40:16 +0900

    Add files via upload

--- C언어 도전 150제/20230410_2/20230410_2/main.cpp
+++ C언어 도전 150제/20230410_2/20230410_2/main.cpp
@@ -1,11 +1,15 @@
 //문제 : 정수형 상수 1과 실수형 상수 3.141592를 더하는 프로그램을 작성하시오.
 #include <stdio.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
 
-void main()
+int main(void)
 {
-	int x = 1;
-	double y = 3.141592;
-	double z = 4.141592;
+	double x = 1;
+	double z = 0;
 
-	printf("%d + %f = %f", x, y, z);
+	z = x + M_PI;
+
+	printf(" x + y = %.6lf", z);
+
 }
