47e0cc3e59b22d96268df3a9ac42ec2efd46f047
seongho2 <125865161+seongho2@users.noreply.github.com>
2023-04-10 20:22:20 +0900

    Add files via upload

---
+++ C언어 도전 150제/20230410_1/20230410_1/main.cpp
@@ -0,0 +1,10 @@
+//문제: 변수 x에 1을 저장하고, 변수 y에 2를 저장하여, x와 y의 합을 z에 저장하는 프로그램을 작성하시오.
+#include <stdio.h>
+
+void main()
+{
+	int x = 1;
+	int y = 2;
+	int z = x + y;
+	printf("%d + %d = %d", x, y, z);
+}
---
+++ C언어 도전 150제/20230410_2/20230410_2/main.cpp
@@ -0,0 +1,11 @@
+//문제 : 정수형 상수 1과 실수형 상수 3.141592를 더하는 프로그램을 작성하시오.
+#include <stdio.h>
+
+void main()
+{
+	int x = 1;
+	double y = 3.141592;
+	double z = 4.141592;
+
+	printf("%d + %f = %f", x, y, z);
+}
