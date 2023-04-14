6638d7a60217e33651fa625a8533e721d8338ce1
seongho2 <125865161+seongho2@users.noreply.github.com>
2023-04-14 16:16:12 +0900

    Add files via upload

--- C언어 도전 150제/20230414/20230414_13/20230414_13/main.cpp
+++ C언어 도전 150제/20230414/20230414_13/20230414_13/main.cpp
@@ -1,13 +1,13 @@
 /*013. 실수형 변수 이해하기(double) 문제 : 실수형 변수 d를 정의하고
 3.141592를 대입하고 출력하는 프로그램을 작성하세요.*/
 #include <stdio.h>
-#define _USE_MATH_DEFINES
+#define PI 3.14159265
 #include <math.h>
 
 void main()
 {
 	double d;
-	d = M_PI;
+	d = PI;
 
 	printf("x =%lf\n", d);
 }
