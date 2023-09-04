f5ba4d4d1d6edc467f946a02ca5448fefbeb35a2
seongho2 <125865161+seongho2@users.noreply.github.com>
2023-09-04 18:19:11 +0900

    Add files via upload

---
+++ 20230904/기초 이론/20230904_10/20230904_10/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	FILE* fp = NULL;
+	int c; 
+	fopen_s(&fp, "text.txt", "wt");
+	fputc('a', fp);
+	fclose(fp);
+	fopen_s(&fp, "text.txt", "rt");
+	c = fgetc(fp);
+	printf("%d: %c\n", c, c);
+	fclose(fp);
+
+	return 0;
+}
